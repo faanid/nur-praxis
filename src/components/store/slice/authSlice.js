5c69fb1f699b5c16f64cfa49ee67ac1efada04b8
faanid <73032767+faanid@users.noreply.github.com>
2024-02-18 20:27:02 +0330

    feat: combine authSlice into slice

---
+++ src/components/store/slice/authSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: true,
+  },
+  reducer: {
+    //actions
+    TOGGLE_AUTH: (state) => {
+      state.isLoggedIn = !state.isLoggedIn;
+    },
+  },
+});
+
+export const { TOGGLE_AUTH } = authSlice.actions;
+export default authSlice.reducer;
